c04734a67fc48dbb1d83fad37832ab664da75fb6
David Duc Lam <davla1907@gmail.com>
2026-02-12 14:49:26 +0100

    refactor(user-media): unify add-media endpoint into /users/{user_id}/media
    Replace separate movie and tvshow POST routes with a single media endpoint that branches on MediaType and delegates persistence through UserMediaService.

--- src/main/java/com/github/davidduclam/movietracker/controller/UserMediaController.java
+++ src/main/java/com/github/davidduclam/movietracker/controller/UserMediaController.java
@@ -1,6 +1,7 @@
 package com.github.davidduclam.movietracker.controller;
 
 import com.github.davidduclam.movietracker.dto.AddUserMediaRequestDTO;
+import com.github.davidduclam.movietracker.model.MediaType;
 import com.github.davidduclam.movietracker.model.UserMedia;
 import com.github.davidduclam.movietracker.service.UserMediaService;
 import org.springframework.web.bind.annotation.*;
@@ -14,16 +15,26 @@ public class UserMediaController {
         this.userMediaService = userMediaService;
     }
 
-    @PostMapping("/users/{user_id}/movie")
-    public UserMedia saveMovieToDb(@PathVariable Long user_id, @RequestBody AddUserMediaRequestDTO addUserMediaRequestDTO) {
-        userMediaService.saveMovieToDb(addUserMediaRequestDTO);
-        return userMediaService.saveUserMediaMovieToDb(user_id, addUserMediaRequestDTO);
-    }
+//    @PostMapping("/users/{user_id}/movie")
+//    public UserMedia saveMovieToDb(@PathVariable Long user_id, @RequestBody AddUserMediaRequestDTO addUserMediaRequestDTO) {
+//        userMediaService.saveMovieToDb(addUserMediaRequestDTO);
+//        return userMediaService.saveUserMediaMovieToDb(user_id, addUserMediaRequestDTO);
+//    }
 
-    @PostMapping("/users/{user_id}/tvshow")
-    public UserMedia saveTvShowToDb(@PathVariable Long user_id, @RequestBody AddUserMediaRequestDTO addUserMediaRequestDTO) {
-        userMediaService.saveTvShowToDb(addUserMediaRequestDTO);
+//    @PostMapping("/users/{user_id}/tvshow")
+//    public UserMedia saveTvShowToDb(@PathVariable Long user_id, @RequestBody AddUserMediaRequestDTO addUserMediaRequestDTO) {
+//        userMediaService.saveTvShowToDb(addUserMediaRequestDTO);
+//        return userMediaService.saveUserMediaTvShowToDb(user_id, addUserMediaRequestDTO);
+//
+//    }
+
+    @PostMapping("/users/{user_id}/media")
+    public UserMedia saveMediaToDb(@PathVariable Long user_id, @RequestBody AddUserMediaRequestDTO addUserMediaRequestDTO) {
+        if (addUserMediaRequestDTO.getMediaType().equals(MediaType.MOVIE)) {
+            userMediaService.saveMovieToDb(addUserMediaRequestDTO);
+        } else {
+            userMediaService.saveTvShowToDb(addUserMediaRequestDTO);
+        }
         return userMediaService.saveUserMediaTvShowToDb(user_id, addUserMediaRequestDTO);
-
     }
 }
